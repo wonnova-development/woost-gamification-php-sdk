2adfd557367e8be5a023c90fbcbbd420d407c9cc
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-02-14 11:26:23 +0100

    Implemented connection methods in Client class

--- src/Connection/Client.php
+++ src/Connection/Client.php
@@ -1,20 +1,24 @@
 <?php
 namespace Wonnova\SDK\Connection;
 
+use GuzzleHttp\Exception\ServerException;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Wonnova\SDK\Auth\CredentialsInterface;
 use Wonnova\SDK\Auth\TokenInterface;
+use Wonnova\SDK\Common\Headers;
 use Wonnova\SDK\Common\URIUtils;
 use Wonnova\SDK\Model\User;
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException;
 
 /**
  * Class Client
  * @author Wonnova
  * @link http://www.wonnova.com
  */
-class Client implements ClientInterface
+class Client extends GuzzleClient implements ClientInterface
 {
     const AUTH_ROUTE = '/auth';
     const USER_AGENT = 'wonnova-php-sdk';
@@ -23,10 +27,6 @@ class Client implements ClientInterface
      * @var CredentialsInterface
      */
     protected $credentials;
-    /**
-     * @var GuzzleClient
-     */
-    protected $httpClient;
     /**
      * @var Serializer
      */
@@ -41,50 +41,94 @@ class Client implements ClientInterface
     protected $language;
 
     /**
-     * @param GuzzleClient $httpClient
-     * @param Serializer $serializer
      * @param CredentialsInterface $credentials
      * @param $language
      */
-    public function __construct(
-        GuzzleClient $httpClient,
-        Serializer $serializer,
-        CredentialsInterface $credentials,
-        $language
-    ) {
-        $this->httpClient = $httpClient;
-        $this->serializer = $serializer;
+    public function __construct(CredentialsInterface $credentials, $language = 'es')
+    {
+        parent::__construct([
+            'base_url' => URIUtils::HOST,
+            'defaults' => [
+                'headers' => [
+                    'User-Agent' => self::USER_AGENT
+                ]
+            ]
+        ]);
+        $this->serializer = new Serializer([new GetSetMethodNormalizer()], [new JsonEncoder()]);
         $this->credentials = $credentials;
         $this->language = $language;
     }
 
     /**
-     * Creates a new Client with provided Credentials and language
+     * Performs a connection to defined endpoint with defined options
      *
-     * @param CredentialsInterface $credentials
-     * @param string $language
-     * @return Client
+     * @param $method
+     * @param $route
+     * @param array $options
+     * @return \GuzzleHttp\Message\ResponseInterface
+     * @throws \Wonnova\SDK\Exception\ServerException
      */
-    public static function factory(CredentialsInterface $credentials, $language = 'es')
+    protected function connect($method, $route, array $options = [])
     {
-        return new Client(
-            new GuzzleClient([
-                'base_url' => sprintf('%s/rest', URIUtils::HOST),
-                'defaults' => [
-                    'headers' => [
-                        'User-Agent' => self::USER_AGENT
-                    ]
-                ]
-            ]),
-            new Serializer([], [new JsonEncoder()]),
-            $credentials,
-            $language
+        try {
+            // Perform authentication if token has not been set yet
+            if (! isset($this->token)) {
+                $this->authenticate();
+            }
+
+            // Add the language and token headers
+            $options = $this->processOptionsWithDefaults($options);
+
+            return $this->send($this->createRequest($method, $route, $options));
+        } catch (ClientException $e) {
+            if ($e->getCode() === 401) {
+                $this->token = null;
+                return $this->connect($method, $route, $options);
+            }
+
+            // TODO Handle other errors
+
+            throw new \Wonnova\SDK\Exception\ClientException(
+                sprintf('There was a client error processing a request to "%s" with method "%s"', $route, $method),
+                $e
+            );
+        } catch (ServerException $e) {
+            throw new \Wonnova\SDK\Exception\ServerException(
+                sprintf('There was a server error processing a request to "%s" with method "%s"', $route, $method),
+                $e
+            );
+        }
+    }
+
+    /**
+     * Performs authentication caching the auth token
+     */
+    protected function authenticate()
+    {
+        $response = $this->send($this->createRequest('POST', URIUtils::parseUri(self::AUTH_ROUTE), [
+            'json' => [
+                'key' => $this->credentials->getKey()
+            ]
+        ]));
+        $this->token = $this->serializer->deserialize(
+            $response->getBody()->getContents(),
+            'Wonnova\SDK\Auth\Token',
+            'json'
         );
     }
 
-    protected function connect()
+    /**
+     * @param array $options
+     * @return array
+     */
+    protected function processOptionsWithDefaults(array $options)
     {
+        $options['headers'] = isset($options['headers']) ? $options['headers'] : [];
 
+        $options['headers'][Headers::LANGUAGE_HEADER] = $this->language;
+        $options['headers'][Headers::TOKEN_HEADER] = $this->token->getToken();
+
+        return $options;
     }
 
     /**
@@ -94,8 +138,7 @@ class Client implements ClientInterface
      */
     public function getUsers()
     {
-        $response = $this->send($this->createRequest('GET', self::USERS_ROUTE));
-        return $this->mapper->mapArray($response->getBody(), [], '');
+        $response = $this->connect('GET', URIUtils::parseUri(self::USERS_ROUTE));
     }
 
     /**
--- src/Model/User.php
+++ src/Model/User.php
@@ -4,6 +4,29 @@ namespace Wonnova\SDK\Model;
 class User implements \JsonSerializable
 {
     /**
+     * @var string
+     */
+    protected $userId;
+
+    /**
+     * @return string
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param string $userId
+     * @return $this
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+        return $this;
+    }
+
+    /**
      * (PHP 5 &gt;= 5.4.0)<br/>
      * Specify data which should be serialized to JSON
      * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
