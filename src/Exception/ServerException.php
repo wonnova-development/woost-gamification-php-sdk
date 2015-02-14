60bda82e1fc7b042032e9195bd29af73206e83b9
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-02-14 11:26:00 +0100

    Created base exception classes

---
+++ src/Exception/ServerException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Wonnova\SDK\Exception;
+
+/**
+ * Class ServerException
+ * @author Wonnova
+ * @link http://www.wonnova.com
+ */
+class ServerException extends \RuntimeException implements ExceptionInterface
+{
+
+}
