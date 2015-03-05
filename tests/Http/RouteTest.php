606b992a64ca2d632c23bfb5095bb89c6335be8e
Alejandro Celaya <acelaya@wonnova.com>
2015-03-05 12:40:39 +0100

    Created Route class to represent routes

---
+++ tests/Http/RouteTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace Wonnova\SDK\Test\Http;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Wonnova\SDK\Http\Route;
+
+/**
+ * Class RouteTest
+ * @author Wonnova
+ * @link http://www.wonnova.com
+ */
+class RouteTest extends TestCase
+{
+    public function testSimpleRoute()
+    {
+        $routePattern = '/foo/bar';
+        $route = new Route($routePattern);
+        $this->assertEquals($routePattern, $route->__toString());
+    }
+
+    public function testRouteWithRouteParams()
+    {
+        $routePattern = '/foo/%userId%/bar/%anotherParam%';
+        $route = new Route($routePattern, [
+            'userId' => '12345',
+            'anotherParam' => 'value'
+        ]);
+        $this->assertEquals('/foo/12345/bar/value', $route->__toString());
+    }
+
+    public function testRouteWithQueryParams()
+    {
+        $routePattern = '/foo/bar';
+        $route = new Route($routePattern, [], [
+            'foo' => 1,
+            'bar' => 2,
+            'name' => 'wonnova'
+        ]);
+        $this->assertEquals('/foo/bar?foo=1&bar=2&name=wonnova', $route->__toString());
+    }
+
+    public function testRouteWithAllParams()
+    {
+        $routePattern = '/foo/%userId%/bar/%anotherParam%';
+        $route = new Route($routePattern, [
+            'userId' => '12345',
+            'anotherParam' => 'value'
+        ], [
+            'foo' => 1,
+            'bar' => 2
+        ]);
+        $this->assertEquals('/foo/12345/bar/value?foo=1&bar=2', $route->__toString());
+    }
+}
